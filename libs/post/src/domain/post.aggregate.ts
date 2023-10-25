5a537ebd3f362ab738798001483e7c28acd2a9b1
Aleksander <asava@sibmail.com>
2023-10-25 19:00:25 +0700

    Второй урок 8:34

---
+++ libs/post/src/domain/post.aggregate.ts
@@ -0,0 +1,17 @@
+import { IPost } from './post.interface'
+import { randomStringGenerator } from '@nestjs/common/utils/random-string-generator.util'
+
+export class PostAggregate implements IPost {
+	id: string = randomStringGenerator()
+	title: string
+	message: string
+	authorId: string
+	published = false
+	createdAt = new Date().toISOString()
+	updatedAt = new Date().toISOString()
+
+	private constructor() {}
+}
+
+/**Второй урок 8:34 */
+// Микросервис на NestJS обеспечивают более гибкую и модульную архитектуру приложения, что может быть особенно полезным в больших и сложных проектах. Они позволяют командам разработчиков разрабатывать, тестировать и масштабировать части приложения независимо друг от друга.
---
+++ libs/post/src/domain/post.interface.ts
@@ -0,0 +1,15 @@
+export interface IPost {
+	id: string
+
+	title: string
+
+	message: string
+
+	authorId: string
+
+	published: Boolean
+
+	createdAt: string
+
+	updatedAt: string
+}
